7cba8be1e15127410d80b46c5412b6f2a114e2cf
Isai Albarran <isaialbarran@gmail.com>
2020-09-21 16:35:13 +0200

    change build to docs

---
+++ docs/precache-manifest.3bf8ed5f9fa49d3422dbac5d087064d6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ebd0d044620ac22c3933cca3c9fac35b",
+    "url": "/index.html"
+  },
+  {
+    "revision": "a4d06cc77904776547ff",
+    "url": "/static/css/main.62873007.chunk.css"
+  },
+  {
+    "revision": "1867717e13a5d075e07f",
+    "url": "/static/js/2.7e82d372.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7e82d372.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a4d06cc77904776547ff",
+    "url": "/static/js/main.69d5725b.chunk.js"
+  },
+  {
+    "revision": "4118261bad4d6485cd59",
+    "url": "/static/js/runtime-main.93e9af04.js"
+  }
+]);
